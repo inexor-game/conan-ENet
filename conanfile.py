8a4b2353d8e19841a73c054863c4845a544844c0
Croydon <cr0ydon@outlook.com>
2017-12-02 02:15:45 +0100

    Fix git checkout

--- conanfile.py
+++ conanfile.py
@@ -8,31 +8,31 @@ class ENetConan(ConanFile):
     options = {"shared": [True, False]}
     description = "Reliable UDP networking library"
     default_options = "shared=False"
-    url = "http://github.com/inexorgame/conan-ENet"
+    url = "https://github.com/inexorgame/conan-ENet"
     license = "MIT"
     generators = "cmake"
 
     def source(self):
-        self.run("git clone https://github.com/lsalzman/enet.git")
+        self.run("git clone https://github.com/lsalzman/enet.git ./")
         self.run("git checkout 39a72ab1990014eb399cee9d538fd529df99c6a0")
         # This small hack might be useful to guarantee proper /MT /MD linkage in MSVC
         # if the packaged project doesn't have variables to set it properly
-        tools.replace_in_file("enet/CMakeLists.txt", "project(enet)", '''project(enet)
+        tools.replace_in_file("CMakeLists.txt", "project(enet)", '''project(enet)
         include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
         conan_basic_setup()''')
         if self.options.shared == "True":
-            tools.replace_in_file("enet/CMakeLists.txt", "add_library(enet STATIC", "add_library(enet SHARED")
+            tools.replace_in_file("CMakeLists.txt", "add_library(enet STATIC", "add_library(enet SHARED")
 
     def config(self):
         del self.settings.compiler.libcxx  # We are a pure C lib.
 
     def build(self):
         cmake = CMake(self)
-        self.run('cmake %s/enet %s' % (self.conanfile_directory, cmake.command_line))
+        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy('*', dst='include', src='enet/include')
+        self.copy('*', dst='include', src='include')
         self.copy("*.lib", dst="lib", src="", keep_path=False)
         self.copy("*.pdb", dst="lib", src="", keep_path=False)
         self.copy("*.a", dst="lib", src="", keep_path=False)
