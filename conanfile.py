6111fdec5789c1a0874a7cdc21f8e304f03aa1ac
Croydon <cr0ydon@outlook.com>
2017-12-28 18:36:30 +0100

    Make recipe compatible with Conan >= 1.0.0

--- conanfile.py
+++ conanfile.py
@@ -28,7 +28,7 @@ class ENetConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
+        self.run('cmake %s %s' % (self.source_folder, cmake.command_line))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
